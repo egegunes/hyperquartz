0d3f07ad0e4ccf55ad8ccf776deae3f4357bb60f
Jacky Zhao <j.zhao2k19@gmail.com>
2026-01-03 21:38:14 +0800

    Quartz sync: Jan 3, 2026, 9:38 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -51,14 +51,14 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
   left,
   right: [
-    Component.Graph({
+    Component.DesktopOnly(Component.Graph({
       localGraph: {
         showTags: false,
       },
       globalGraph: {
         showTags: false,
       },
-    }),
+    })),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
